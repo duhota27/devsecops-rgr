07c11bb848f5361a7fd7f8437c400ac835bb310f
duhota27 <uta.tuk2003@gmail.com>
2026-05-29 10:31:06 +1000

    Add password hashing demo

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+    "crypto/sha256"
     "fmt"
     "net/http"
 )
+
+func hashPassword(password string) string {
+    hash := sha256.Sum256([]byte(password))
+    return fmt.Sprintf("%x", hash)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "DevSecOps RGR App is running")
 }
 
 func main() {
+    // имитация "пароля системы"
+    password := "admin123"
+    hashed := hashPassword(password)
+
+    // вывод в консоль
+    fmt.Println("Original password:", password)
+    fmt.Println("Hashed password (SHA-256):", hashed)
+
     http.HandleFunc("/", handler)
+
     fmt.Println("Server started on :8080")
     http.ListenAndServe(":8080", nil)
 }
